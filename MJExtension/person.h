e772baa7f52d80b012d085a1e06988528f63c593
doubi <xuting@163.com>
2016-08-21 16:14:24 +0800

    添加了person 类

---
+++ MJExtension/person.h
@@ -0,0 +1,13 @@
+//
+//  person.h
+//  MJExtensionExample
+//
+//  Created by mac on 16/8/21.
+//  Copyright © 2016年 小码哥. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface person : NSObject
+
+@end
---
+++ MJExtension/person.m
@@ -0,0 +1,14 @@
+
+//
+//  person.m
+//  MJExtensionExample
+//
+//  Created by mac on 16/8/21.
+//  Copyright © 2016年 小码哥. All rights reserved.
+//
+
+#import "person.h"
+
+@implementation person
+
+@end
